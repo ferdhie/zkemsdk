e513dbe4b5423a366aa9c297958def0aaed25749
908311595@qq.com <908311595@qq.com>
2014-12-18 15:57:21 +0800

    中控考勤TFT系列代码封装

--- src/com/zkem/tft/ZkemSDK.java
+++ src/com/zkem/tft/ZkemSDK.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import com.charset.CharsetUtils;
 import com.jacob.activeX.ActiveXComponent;
 import com.jacob.com.Variant;
